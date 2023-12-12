ac1632dbaa35ee1365d107c9e981ae150d2441fb
unknown <youngscientist.iysa@gmail.com>
2023-12-12 10:44:51 +0700

    Persiapan 2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,6 @@ import { HeaderComponent } from "../app/components/layout/header/header.componen
 import { FooterComponent } from "../app/components/layout/footer/footer.component";
 import { DigitalAgencyTwoComponent } from "../app/components/pages/digital-agency-two/digital-agency-two.component";
 import { DigitalAgencyThreeComponent } from "../app/components/pages/digital-agency-three/digital-agency-three.component";
-import { ProjectsComponent } from "../app/components/pages/projects/projects.component";
-import { ProjectDetailsComponent } from "../app/components/pages/project-details/project-details.component";
-import { ServicesComponent } from "../app/components/pages/services/services.component";
-import { ServiceDetailsComponent } from "../app/components/pages/service-details/service-details.component";
-import { BlogComponent } from "../app/components/pages/blog/blog.component";
-import { BlogDetailsComponent } from "../app/components/pages/blog-details/blog-details.component";
-import { TeamComponent } from "../app/components/pages/team/team.component";
-import { TestimonialsComponent } from "../app/components/pages/testimonials/testimonials.component";
-import { AboutComponent } from "../app/components/pages/about/about.component";
-import { ContactComponent } from "../app/components/pages/contact/contact.component";
-import { BusinessStartupsComponent } from "../app/components/pages/business-startups/business-startups.component";
-import { SeoAgencyComponent } from "../app/components/pages/seo-agency/seo-agency.component";
 import { FAQComponent } from "../app/components/pages/faq/faq";
 
 @NgModule({
@@ -28,18 +16,6 @@ import { FAQComponent } from "../app/components/pages/faq/faq";
         FooterComponent,
         DigitalAgencyTwoComponent,
         DigitalAgencyThreeComponent,
-        ProjectsComponent,
-        ProjectDetailsComponent,
-        ServicesComponent,
-        ServiceDetailsComponent,
-        BlogComponent,
-        BlogDetailsComponent,
-        TeamComponent,
-        TestimonialsComponent,
-        AboutComponent,
-        ContactComponent,
-        BusinessStartupsComponent,
-        SeoAgencyComponent,
         FAQComponent,
     ],
     imports: [BrowserModule, AppRoutingModule, CommonModule],
